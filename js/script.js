62cbf9e9e14d0add594e2bf9398c9261e0a7c7e5
Mark <marksaitiev@gmail.com>
2023-02-24 15:54:20 +0000

    задачи и задачи со звездочкой*

--- js/script.js
+++ js/script.js
@@ -1,29 +1,75 @@
-"use strict";
-
-let num = 50;
-
-
-// while (num <= 55) { // цикл Пока переменная нам будет меньше 55 будут выполнятся определенные действия
-//     console.log(num);
-//     num++;
-// };
-
-
-// do {
-//     console.log(num);
-//     num++;
-// }
-// while (num <= 55);
-
-
-// КОМБИНИРУЕМ С УСЛОВИЕМ . если наша переменная i будет равна 6 , расскрываем условия и ставим брейк
-// break - оператор который полностью прирывает цикл   ,дальше не продолжается 
-for (let i = 1; i < 10; i++) { 
-    if(i === 6) {
-        // break; 
-        continue; // оператор который  позволяет пропустить тот шаг который нам не нужен , 
-                  // но при этом не прирывает полностью цикл 
-
+for (i = 20; i > 10; i--) {
+    console.log(i);
+    if (i === 14) {
+        break;
     }
+};
+
+let i = 4;
+do {
+    i++;
     console.log(i);
 }
+while (i<10);
+    
+    
+for (j=2; j<=10;j++) {
+     if(j % 2 !== 0) {
+        continue;
+     } 
+     console.log(j);
+};
+
+
+
+const obj = {
+    name: ' john',
+    age: 25
+};
+console.log(obj);
+
+
+// MACUB
+
+const arrayOfNumbers = [];
+
+for (let i = 5; i < 11; i++) {
+        arrayOfNumbers[i - 5] = i;
+    }
+
+console.log(arrayOfNumbers);
+return arrayOfNumbers;
+
+
+let arr = [5,6,7,8,9,10];
+console.log(arr);
+
+
+
+
+
+
+let i = 2;
+while (i <= 15) {
+    i++;
+    if (i % 2 === 0) {
+    continue;
+    } else {
+    console.log(i);
+    }
+}
+
+
+
+
+"use strict";
+for (let i = 0; i < 3; i++) {
+    console.log(`first lvl `);
+    for(let j = 0; j < 3; j++) {
+        console.log(`second lvl `);
+        for(let k = 0; k < 3; k++) {
+            if 
+            console.log(`third lvl `);
+        }
+    }
+};
